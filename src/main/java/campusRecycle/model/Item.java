f11e2d030e7debbbb3df68e4151f5273a788270d
Albert Dayn <Albert.Dayn@colorado.edu>
2018-03-20 16:17:49 -0600

    Added fields to Item class

--- src/main/java/campusRecycle/model/Item.java
+++ src/main/java/campusRecycle/model/Item.java
@@ -9,22 +9,81 @@ public class Item {
     @GeneratedValue(strategy=GenerationType.AUTO)
     private int id;
 
-    private String name;
+    private String title;
+
+    private String description;
+
+    private String imagePath;
+
+    private double price;
+
+    // Enum TODO
+//    private ItemState state;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(referencedColumnName = "name")
+    private User seller;
+
+    @ManyToOne
+    @JoinColumn(referencedColumnName = "name")
+    private User buyer;
+
+    /////////////////////////////////////////////////////////////
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    public void setImagePath(String imagePath) {
+        this.imagePath = imagePath;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public User getSeller() {
+        return seller;
+    }
+
+    public void setSeller(User seller) {
+        this.seller = seller;
+    }
+
+    public User getBuyer() {
+        return buyer;
+    }
+
+    public void setBuyer(User buyer) {
+        this.buyer = buyer;
+    }
+
     public int getId() {
         return id;
     }
 
-    /////////////////////////////////////////////////////////////
-
     public void setId(int id) {
         this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 }
