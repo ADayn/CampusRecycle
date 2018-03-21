928eeeb7b79f6d6d9435c107ea19dabea45352fe
Yang Jiang <yaji5342@colorado.edu>
2018-03-21 14:52:50 -0600

    updated user class

--- src/main/java/campusRecycle/model/User.java
+++ src/main/java/campusRecycle/model/User.java
@@ -1,3 +1,10 @@
+package campusRecycle.model;
+
+import campusRecycle.model.Item;
+
+import java.awt.event.ItemEvent;
+import java.util.List;
+
 public class User extends Person{
 	List<Integer> ratings;
 	 
