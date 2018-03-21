b68ef067e437526a6f03ef07cd37fe9c124a9b7d
Yang Jiang <yaji5342@colorado.edu>
2018-03-21 14:52:22 -0600

    updated admin class

--- src/main/java/campusRecycle/model/Admin.java
+++ src/main/java/campusRecycle/model/Admin.java
@@ -1,3 +1,10 @@
+package campusRecycle.model;
+
+import campusRecycle.model.Item;
+
+import java.awt.event.ItemEvent;
+import java.util.List;
+
 public class Admin extends Person{
 	User user;
 	Item it;
