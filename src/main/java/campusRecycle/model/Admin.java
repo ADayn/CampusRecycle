d70a082f75799060e79df0f5542fb2b23ba3fd70
Yang Jiang <yaji5342@colorado.edu>
2018-03-21 14:48:51 -0600

    added admin class

---
+++ src/main/java/campusRecycle/model/Admin.java
@@ -0,0 +1,16 @@
+public class Admin extends Person{
+	User user;
+	Item it;
+	
+	public void sendApprovalEmail(User user,Item it) {
+		
+	}
+	
+	public void sendDenialEmail(User user,Item it) {
+		
+	}
+	
+	public void sendUserDeletionEmail(User user) {
+		
+	}
+}
