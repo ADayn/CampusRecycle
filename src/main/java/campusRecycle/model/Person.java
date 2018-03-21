3feedb4656d092585376010ff0c4eb7af8490582
Si Shen <shensilasg@gmail.com>
2018-03-21 14:55:49 -0600

    add setters getters for Person

--- src/main/java/campusRecycle/model/Person.java
+++ src/main/java/campusRecycle/model/Person.java
@@ -14,5 +14,29 @@ public class Person {
 	public void sendEmail(Person person, String msg) {
 		
 	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public char[] getPassword() {
+		return password;
+	}
+
+	public void setPassword(char[] password) {
+		this.password = password;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
 }
 
